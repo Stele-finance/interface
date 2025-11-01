cdf1b1b92984681c4d68ab0aca73b945c3d6adbf
ilotod <openfund365@gmail.com>
2025-11-01 12:46:09 +0900

    refactor: Simplify Dashboard and Funds components by removing unused elements
    - Removed the DashboardCharts component from the Dashboard page to streamline the layout.
    - Refactored the DashboardClientComponents to eliminate the activeTab state and related logic, focusing solely on the Funds component.
    - Updated the Funds component to enhance the display of fund information, including profit ratio and days since creation, while removing unnecessary props and improving overall clarity.

--- app/dashboard/fund/page.tsx
+++ app/dashboard/fund/page.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import { DashboardClientComponents } from "./components/DashboardClientComponents"
-import { DashboardCharts } from "./components/DashboardCharts"
 import { useState, useEffect } from "react"
 
 export default function Dashboard() {
@@ -24,7 +23,6 @@ export default function Dashboard() {
   return (
     <div className="w-full mx-auto px-3 py-0 sm:px-6 sm:py-2 overflow-hidden">
       <div className="max-w-6xl mx-auto space-y-4">
-        <DashboardCharts network={selectedNetwork} />
         <div className="mt-4 sm:mt-6">
           <DashboardClientComponents
             network={selectedNetwork}
